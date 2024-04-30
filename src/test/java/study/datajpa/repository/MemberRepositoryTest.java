d870bf1bd5b1e40f6d5fd62c34a7d30bed2569e5
gunners <chakagae123@naver.com>
2024-04-30 22:23:36 +0900

    * 섹션 1. 스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+//@Rollback(value = false) -> 롤백하고 싶지않을때(쿼리를 보고싶을 때)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member saveMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+}
