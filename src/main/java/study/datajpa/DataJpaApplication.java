536738802da4699a850a838d7ff6cbea478e7859
gunners <chakagae123@naver.com>
2024-05-02 17:13:45 +0900

    * 섹션 3. 공통 인터페이스 설정

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,9 +1,12 @@
 package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository") //를 해줘야 하지만 부트가 알아서 해준다
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
