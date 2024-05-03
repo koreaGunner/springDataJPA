1468e22333beb6c148edb35793cf968b7b726401
gunners <chakagae123@naver.com>
2024-05-03 22:34:40 +0900

    * 섹션 4. 반환 타입

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -28,4 +29,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String username); //컬렉션 -> !!조회결과가 없더라고 null로 반환하지 않는다(empty collection으로 반환)
+    Member findMemberByUsername(String username); //단건 -> !!조회결과가 없으면 null로 반환(JPA는 NoResultException이 뜬다. spring data JPA로 인해서 null 반환)
+    Optional<Member> findOptionalByUsername(String username); //단건 Optional -> 단건 조회는 optional이 아니더라도 단건 조회가 아닐때 오류가 뜬다
 }
